b2cbf0dd7eb87a3ddb1bac06e99ed663b70dfc49
chuckyNoon <levchenko-dima00@inbox.ru>
2020-07-02 14:26:51 +0300

    cleaned code

--- app/src/main/java/com/example/bookanalyzer/Analysis.kt
+++ app/src/main/java/com/example/bookanalyzer/Analysis.kt
@@ -5,75 +5,78 @@ import android.content.Intent
 import android.os.AsyncTask
 import android.widget.TextView
 import java.io.InputStream
+import java.text.Normalizer
 import kotlin.math.roundToInt
 
 
 class BookAnalysis(private val inStream: InputStream, val path:String, val ctx:Context)
 
 {
-    private lateinit var parser:FileParser
-    private var normalizer: WordNormalizer? = null
-
-    private var simpleText:String? = null
-    lateinit var finalMap:Map<String,Int>
     var avgWordLen:Double = 0.0
     var avgSentenceLen:Double= 0.0
-    var wordsCount = 0
-    var unWordsCount = 0
+    var wordCount = 0
+    var uniqWordCount = 0
     var img:ByteArray? = null
+    lateinit var normalizedWordMap:Map<String,Int>
 
     fun doit() {
-        parser = FileParser()
-        normalizer = WordNormalizer(ctx)
-        simpleText = when{
-            path.contains(".txt") -> parser.parseTxt(inStream)
-            path.contains(".epub") -> parser.epubToTxt(inStream)
-            path.contains(".fb2") ->parser.fb2ToTxt(inStream)
-            else -> parser.parseTxt(inStream)
+        val parser = BookParser()
+        val time1 = System.currentTimeMillis()
+        val normalizer = WordNormalizer(ctx)
+        val time2 = System.currentTimeMillis()
+        val simpleText = parser.parseFile(inStream, path)?:""
+        if (simpleText.isEmpty()){
+            normalizedWordMap = mapOf()
+            return
         }
+        val time3 = System.currentTimeMillis()
         img = parser.img
-        finalMap = normalizeWordMap(parser.parseWords(simpleText!!))
-        calcWordCount()
-        calcAvgWordLen()
-        calcAvgSentenceLen()
-        unWordsCount = finalMap.size
-        println(unWordsCount)
+        val sourceWordMap = parser.parseWords(simpleText)
+        normalizedWordMap = normalizeWordMap(normalizer, sourceWordMap)
+        val time4 = System.currentTimeMillis()
+        calcWordCount(sourceWordMap)
+        calcAvgWordLen(sourceWordMap)
+        calcAvgSentenceLen(simpleText)
+        val time5 = System.currentTimeMillis()
+        uniqWordCount = normalizedWordMap.size
+        println("t norm=" + ((time2- time1).toDouble() / 1000 ).toString())
+        println("t pars=" + ((time3- time2).toDouble() / 1000 ).toString())
+        println("t normmap=" + ((time4- time3).toDouble() / 1000 ).toString())
+        println("t last=" + ((time5- time4).toDouble() / 1000 ).toString())
+        println("t all=" + ((time5- time1).toDouble() / 1000 ).toString())
+        println(uniqWordCount)
     }
 
-    private fun calcWordCount(){
+    private fun calcWordCount(sourceWordMap: MutableMap<String, Int>){
         var ans:Int = 0
-        finalMap.let {
-            for ((a, b) in it) {
-                ans += b
-            }
+        for ((word, count) in sourceWordMap) {
+            ans += count
         }
-        wordsCount = ans
+        wordCount = ans
+    }
+
+    private fun calcAvgWordLen(sourceWordMap: MutableMap<String, Int>){
+        var sumLen:Long = 0
+        for ((a, b) in sourceWordMap) {
+            sumLen += a.length * b
+        }
+        avgWordLen = if (wordCount != 0)  roundDouble(sumLen.toDouble()/ wordCount) else 0.0
+    }
+
+    private fun calcAvgSentenceLen(simpleText:String){
+        val sentences = simpleText.split(".")
+        val sentenceCount = sentences.size
+        avgSentenceLen = if (sentenceCount != 0) roundDouble(wordCount.toDouble() / sentenceCount) else 0.0
     }
 
     private fun roundDouble(d:Double):Double{
         return ((d * 100).roundToInt().toDouble() / 100)
     }
 
-    private fun calcAvgWordLen(){
-        var sumLen:Long = 0
-        finalMap.let {
-            for ((a, b) in it) {
-                sumLen += a.length * b
-            }
-        }
-        avgWordLen = if (wordsCount != 0)  roundDouble(sumLen.toDouble()/ wordsCount) else 0.0
-    }
-
-    private fun calcAvgSentenceLen(){
-        val strs = simpleText?.split(".")
-        val sentencesCount = strs?.size?:0
-        avgSentenceLen = if (sentencesCount != 0) roundDouble(wordsCount.toDouble() / sentencesCount) else 0.0
-    }
-
-    private fun normalizeWordMap(sourceMap:MutableMap<String,Int>):Map<String,Int>{
+    private fun normalizeWordMap(normalizer:WordNormalizer, sourceMap:MutableMap<String,Int>):Map<String,Int>{
         val ansMap = mutableMapOf<String,Int>()
         for ((a, b) in sourceMap){
-            val newWord = normalizer?.getLemma(a)
+            val newWord = normalizer.getLemma(a)
             if (newWord == null){
                 ansMap[a] = (ansMap[a]?:0) + b
             }else{
--- app/src/main/java/com/example/bookanalyzer/StartActivity.kt
+++ app/src/main/java/com/example/bookanalyzer/StartActivity.kt
@@ -25,7 +25,6 @@ class StartActivity : AppCompatActivity() {
     private lateinit var linLayout:LinearLayout
     private lateinit var tableLayout:TableLayout
     private  var bookCount = 0
-    private var currBookInd = 0
     private var tableWidth = 0
     private var tableHeight = 0
 
@@ -34,13 +33,13 @@ class StartActivity : AppCompatActivity() {
         setContentView(R.layout.activity_start)
 
         linLayout = findViewById(R.id.linLayout)
-        bookCount = readCount()
+        bookCount = getBookCount()
         createTable()
     }
 
     override fun onStart() {
         super.onStart()
-        val newBookCount = readCount()
+        val newBookCount = getBookCount()
         if (newBookCount != bookCount) {
             linLayout.removeView(tableLayout)
             bookCount = newBookCount
@@ -58,18 +57,17 @@ class StartActivity : AppCompatActivity() {
         tableLayout.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
             LinearLayout.LayoutParams.WRAP_CONTENT)
 
-        currBookInd = 0
         var lastRow:TableRow? = null
         for (i in 0 until tableHeight){
-            lastRow = createRow()
+            lastRow = createRow(i)
             tableLayout.addView(lastRow)
         }
-        val selectButton = createSelectButton()
-        lastRow?.addView(selectButton)
+        val addButton = createAddButton()
+        lastRow?.addView(addButton)
         linLayout.addView(tableLayout)
     }
 
-    private fun createRow() : TableRow{
+    private fun createRow(rowNumber:Int) : TableRow{
         val tableRow = TableRow(this).apply {
             layoutParams = TableLayout.LayoutParams(
                 TableLayout.LayoutParams.MATCH_PARENT,
@@ -77,38 +75,34 @@ class StartActivity : AppCompatActivity() {
                 setMargins(0,0,0,0)
             }
             gravity = Gravity.TOP
-
         }
+        var currBookInd = rowNumber * tableWidth
         for (j in 0 until tableWidth){
             if (currBookInd == bookCount)
                 break
-            tableRow.addView(createField())
+            tableRow.addView(createBookButton(currBookInd))
+            currBookInd++
         }
         return (tableRow)
     }
 
-    private fun dpToPx(dp:Int):Int{
-        val scale: Float = this.resources.displayMetrics.density
-        return (dp * scale + 0.5f).toInt()
-    }
-
-    private fun createField() :Button{
+    private fun createBookButton(currBookInd:Int) :Button{
         val button = Button(this).apply{
-            val img =  getImg("img$currBookInd")
+            val img =  getBookImg("img$currBookInd")
             if (img != null) {
                 val bitmapDrawable = BitmapDrawable(resources, img)
                 background = bitmapDrawable
             }else {
-                    val name = getName("info$currBookInd")
-                    text = if (name != null && name.length > 20)
-                        name.substring(0..20) + "..." else name
-                }
+                val name = getBookName("info$currBookInd")
+                text = if (name != null && name.length > 20)
+                    name.substring(0..20) + "..." else name
+            }
             val size = Point()
             windowManager.defaultDisplay.getSize(size)
-            val margin= dpToPx(16)
-            val w = size.x / tableWidth - margin
-            val h = (w * 1.5).toInt()
-            val lytParams = TableRow.LayoutParams(w, h)
+            val margin = dpToPx(16)
+            val btnWidth = size.x / tableWidth - margin
+            val btnHeight = (btnWidth * 1.5).toInt()
+            val lytParams = TableRow.LayoutParams(btnWidth, btnHeight)
             lytParams.setMargins(margin / 2, margin, margin / 2, margin/2)
             layoutParams = lytParams
             elevation = 8f
@@ -118,60 +112,56 @@ class StartActivity : AppCompatActivity() {
 
         button.setOnClickListener {
             val btn:Button = it as Button
-            val ind1 = btn.id
-            val intent1 = Intent(this, MainActivity::class.java)
-            intent1.putExtra("listPath", "list$ind1")
-            intent1.putExtra("imgPath", "img$ind1")
-            intent1.putExtra("infoPath", "info$ind1")
-            startActivity(intent1)
+            val bookInd = btn.id
+            val intentToBook = Intent(this, MainActivity::class.java)
+            intentToBook.putExtra("listPath", "list$bookInd")
+            intentToBook.putExtra("imgPath", "img$bookInd")
+            intentToBook.putExtra("infoPath", "info$bookInd")
+            startActivity(intentToBook)
         }
-        currBookInd++
         return (button)
     }
 
-
-    private fun createSelectButton() : Button{
-        val selectButton = Button(this)
-        selectButton.text = "+"
-
-        val t = TableRow.LayoutParams(dpToPx(70),dpToPx(70))//TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT)
-        t.setMargins(dpToPx(10),dpToPx(10),dpToPx(10),dpToPx(10))
-        t.gravity = Gravity.CENTER_VERTICAL
-        selectButton.layoutParams = t
-        selectButton.setOnClickListener {
+    private fun createAddButton() : Button{
+        val addButton = Button(this).apply {
+            val lytParams = TableRow.LayoutParams(dpToPx(70),dpToPx(70))//TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT)
+            lytParams.setMargins(dpToPx(10),dpToPx(10),dpToPx(10),dpToPx(10))
+            gravity = Gravity.CENTER
+            layoutParams = lytParams
+            text = "+"
+        }
+        addButton.setOnClickListener {
             val intent:Intent = Intent()
                 .setType("*/*")
                 .setAction(Intent.ACTION_GET_CONTENT);
             startActivityForResult(Intent.createChooser(intent, "Select a file"), 123)
         }
-        return (selectButton)
+        return (addButton)
     }
 
-    private fun readCount() : Int{
+    private fun getBookCount() : Int{
         var count = 0
         try{
             val scanner = Scanner(openFileInput("all"))
             count = scanner.nextInt()
             scanner.close()
-            println("f$count")
         }catch (e: IOException){
             println("file not created nor found")
         }
         return (count)
     }
 
-
     @RequiresApi(Build.VERSION_CODES.O)
-    private fun getImg(path:String) : Bitmap?{
+    private fun getBookImg(path:String) : Bitmap?{
         return try {
-            val f = openFileInput(path)
-            val byteArray = f.readBytes()
+            val fileInput = openFileInput(path)
+            val byteArray = fileInput.readBytes()
             val base64Array = String(byteArray,StandardCharsets.UTF_8)
             try {
-                val newByteArray = Base64.getMimeDecoder().decode(base64Array)
-                val bmp = BitmapFactory.decodeByteArray(newByteArray, 0, newByteArray.size)
+                val decodedByteArray = Base64.getMimeDecoder().decode(base64Array)
+                val bmp = BitmapFactory.decodeByteArray(decodedByteArray, 0, decodedByteArray.size)
                 (bmp)
-            }catch (e:java.lang.IllegalArgumentException){
+            }catch (e:java.lang.IllegalArgumentException){//if not in base64
                 val bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
                 (bmp)
             }
@@ -180,7 +170,7 @@ class StartActivity : AppCompatActivity() {
         }
     }
 
-    private fun getName(path: String) : String?{
+    private fun getBookName(path: String) : String?{
         return try {
             val fileInput = openFileInput(path)
             val scanner = Scanner(fileInput)
@@ -192,13 +182,19 @@ class StartActivity : AppCompatActivity() {
         }
     }
 
+    private fun dpToPx(dp:Int):Int {
+        val scale: Float = this.resources.displayMetrics.density
+        return (dp * scale + 0.5f).toInt()
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == 123 && resultCode == Activity.RESULT_OK) {
             val strUri = data?.data.toString()
             val intent = Intent(this, LoaderActivity::class.java)
-            intent.putExtra("uri",strUri)
-            intent.putExtra("ind", bookCount)
+            val newBookInd = bookCount
+            intent.putExtra("uri", strUri)
+            intent.putExtra("ind", newBookInd)
             startActivity(intent)
         }
     }
